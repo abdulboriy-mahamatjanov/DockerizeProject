2c327461238f902598c1c8bdfab3d218fef5b3e8
Abdulboriy <abdulborimahamatjanov86@gmail.com>
2025-04-03 23:27:48 +0600

    app.module file has updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
+import { RegionModule } from './region/region.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), PrismaModule, RegionModule],
   controllers: [AppController],
   providers: [AppService],
 })
+
 export class AppModule {}
