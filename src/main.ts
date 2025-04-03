360fd10f62889ebd8e493f83d43bf17b23524a76
Abdulboriy <abdulborimahamatjanov86@gmail.com>
2025-04-03 23:28:26 +0600

    for main.ts file added to swagger and config for .env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Dockerizing Region Full Crud')
+    .setDescription(
+      'The Region Dockerizing CRUD, and Testing the project API description',
+    )
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  await app.listen(process.env.PORT ?? 3000, () =>
+    console.log(`Server started on port: ${process.env.PORT}`),
+  );
 }
+
 bootstrap();
